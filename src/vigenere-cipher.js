89a563e2637b123ddc8d3a2c37d61a83bea4ac4b
Mayumayumau <mayasidorova@yahoo.com>
2021-03-11 21:22:24 +0800

    add: vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,90 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  direction = true;
+  constructor(mode){
+    this.direction = mode;
+
+  }
+  encrypt(message, key) {
+    
+    let codedMessage = '';
+    let add = '';
+
+    //adjust key and message length
+    let cleanLength = message.length;
+    if (/\W/g.test(message)) cleanLength = message.length - message.match(/\W/g).length;
+    if (key.length < cleanLength){
+      for (let i = key.length; i < cleanLength; i++){
+        add += key[i % key.length];
+      }
+      key += add;
+    }
+    
+    //ENCODING
+
+    message = message.split('');
+
+    let messageIndex, keyIndex;
+    for (let i = 0; i < message.length; i++){
+      while (!/[a-z]/i.test(message[i])){
+        codedMessage += message[i];
+        message.splice(i, 1);
+        if (!message[i]) break;
+      }
+      if (message[i]){
+        messageIndex = this.alphabet.indexOf(message[i].toUpperCase());
+        keyIndex = this.alphabet.indexOf(key[i].toUpperCase());
+        
+        codedMessage += this.alphabet[(messageIndex + keyIndex) % 26];
+      }
+
+    }
+    //ABOVE IS VALID
+
+    if (this.direction == false) codedMessage = codedMessage.split('').reverse().join('')
+    return codedMessage;
+
+    
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    let decodedMessage = '';
+    let add = '';
+    
+
+    //adjust key and message length
+    let cleanLength = message.length;
+    if (/\W/g.test(message)) cleanLength = message.length - message.match(/\W/g).length;
+    if (key.length < cleanLength){
+      for (let i = key.length; i < cleanLength; i++){
+        add += key[i % key.length];
+      }
+      key += add;
+    }
+    
+    //ENCODING
+
+    message = message.split('');
+
+    let messageIndex, keyIndex;
+    for (let i = 0; i < message.length; i++){
+      while (!/[a-z]/i.test(message[i])){
+        decodedMessage += message[i];
+        message.splice(i, 1);
+        if (!message[i]) break;
+      }
+      if (message[i]){
+        messageIndex = this.alphabet.indexOf(message[i].toUpperCase());
+        keyIndex = this.alphabet.indexOf(key[i].toUpperCase());
+        
+        decodedMessage += this.alphabet[(messageIndex - keyIndex + 26) % 26];
+      }
+
+    }
+    //ABOVE IS VALID
+    if (this.direction == false) decodedMessage = decodedMessage.split('').reverse().join('');    
+    return decodedMessage;
   }
 }
 
